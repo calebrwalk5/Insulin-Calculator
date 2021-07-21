886866efea37957784cbd3c3b02ee26d7a68de92
Deniied <entity493@gmail.com>
2021-07-20 20:36:34 -0600

    Create InsulinCalc.cpp

---
+++ InsulinCalc.cpp
@@ -0,0 +1,33 @@
+// InsulinCalc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+// data
+int carbs;
+int sugar;
+int InsulinCarbRatio;
+int goal;
+int SFactor;
+
+//main
+int main()
+{
+    std::cout << "How many carbs are you/were you eating?\n";
+    std::cin >> carbs;
+
+    std::cout << "What is your Glucose Level (before eating)?\n";
+    std::cin >> sugar;
+
+    std::cout << "How many carbs per unit?\n";
+    std::cin >> InsulinCarbRatio;
+
+    std::cout << "What is your sensitivity factor?\n";
+    std::cin >> SFactor;
+
+    std::cout << "What is your goal glucose level?\n";
+    std::cin >> goal;
+
+    std::cout << (carbs/InsulinCarbRatio)+((sugar-goal)/SFactor);
+}
